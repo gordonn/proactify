c12dfd6949174bf2047ba5479d6b5775a23e4d79
gordonn <tofugordon294@gmail.com>
2025-08-22 02:43:11 -0700

    update

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,12 @@
 const close = document.getElementById('close');
 const expand = document.getElementById('expand');
-
 const nav = document.querySelector('nav');
 
 close.addEventListener('click', () => {
-  nav.classList.remove("active"); 
+  nav.classList.remove("active");
 });
 
 expand.addEventListener('click', () => {
-  nav.classList.add("active"); 
+  nav.classList.add("active");
 });
+
